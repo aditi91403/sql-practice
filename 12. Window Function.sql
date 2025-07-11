998f7fa3022f5acc15f6d7f09b20dc87703c6a5c
YesitsAditi <aditiagrawal01264@gmail.com>
2025-07-11 08:37:27 +0530

    Update 12. Window Function.sql
    Add new SQL practice files

--- 12. Window Function.sql
+++ 12. Window Function.sql
@@ -34,3 +34,11 @@ FROM employee_demographics dem
 JOIN employee_salary sal
 	ON dem.employee_id = sal.employee_id;
 
+
+SELECT dem.employee_id,dem.first_name, dem.last_name, gender, salary,
+ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC) AS row_num,
+RANK() OVER(PARTITION BY gender ORDER BY salary DESC) rank_num
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;  
+
